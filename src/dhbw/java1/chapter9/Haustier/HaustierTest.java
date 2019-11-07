40c8ab8b03ceb15330e702d1663fe1208a05e399
Patri <Patri@141.72.138.94>
2019-11-07 16:55:34 +0100

    Added classes Haustier

---
+++ src/dhbw/java1/chapter9/Haustier/HaustierTest.java
@@ -0,0 +1,12 @@
+package dhbw.java1.chapter9.Haustier;
+
+public class HaustierTest {
+
+	public static void main(String[] args) {
+		
+		System.out.println(new VierbeinigesHaustier("Hugo"));
+		System.out.println(new ZweibeinigesHaustier("Bernd"));
+
+	}
+
+}
---
+++ src/dhbw/java1/chapter9/Haustier/ZweibeinigesHaustier.java
@@ -0,0 +1,8 @@
+package dhbw.java1.chapter9.Haustier;
+
+public class ZweibeinigesHaustier extends Haustier {
+
+	public ZweibeinigesHaustier(String name) {
+		super(name, 2);
+	}
+}
