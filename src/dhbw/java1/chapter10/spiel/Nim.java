55c9bb88bf5f6a634544d084e8343a2dc7e2f9e7
Patri <Patri@141.72.130.214>
2019-11-18 16:02:02 +0100

    Added Nim Spiel

---
+++ src/dhbw/java1/chapter10/spiel/Nim.java
@@ -0,0 +1,40 @@
+package dhbw.java1.chapter10.spiel;
+
+public class Nim extends Spiel {
+    private static final int START = 50;            // Anzahl zu Beginn
+    private static final int MAX = 10;              // max. Anzahl für einen Zug
+
+    private int anzahl = START;                     // aktuelle Anzahl
+
+    public Nim() {
+        super(2);                                   // Nim ist ein Zweipersonenspiel
+    }
+
+    @Override
+    protected int macheZug(int spieler) {
+        System.out.print("Anzahl: " + anzahl + ", Spieler #" + spieler + " (1-" + MAX + ")? ");
+        int zug;
+        do {
+            zug = scanner.nextInt();
+        } while (zug < 1 || zug > MAX);             // wdh. wenn Eingabe ungültig
+        anzahl -= zug;                              // Anzahl reduzieren
+        return 1;                                   // der andere Spieler ist dran
+    }
+
+    @Override
+    protected int bestimmeErgebnis(int spieler) {
+        if (anzahl < 1) {                           // letzter entfernt?
+            return spieler;                         // aktueller Spieler hat gewonnen
+        }
+        return OFFEN;
+    }
+
+    @Override
+    protected void druckeErgebnis(int ergebnis) {
+        System.out.println("Spieler #" + ergebnis + " hat gewonnen.");
+    }
+
+    public static void main(String[] args) {
+        new Nim().starteSpiel();                    // neues Nim-Spiel starten
+    }
+}
---
+++ src/dhbw/java1/chapter10/spiel/Spiel.java
@@ -0,0 +1,63 @@
+package dhbw.java1.chapter10.spiel;
+
+import java.util.Scanner;
+
+/**
+ * Ein abstraktes Spiel, das als Superklasse für konkrete Spiele dient - geeignet
+ * sind zugbasierte Ein- oder Mehrpersonenspiele wie Nim, Mau-Mau oder Tic Tac Toe.
+ * Die Ausgabe des aktuellen Spielstands, die Eingabe der Spielzüge und
+ * die Ausgabe des Ergebnisses erfolgt über die Konsole.
+ */
+public abstract class Spiel {
+    protected static final int OFFEN = -2, UNENTSCHIEDEN = -1;
+
+    protected final Scanner scanner = new Scanner(System.in);
+    protected final int spielerAnzahl;
+    private int ergebnis = OFFEN;                           // OFFEN, UNENTSCHIEDEN oder Gewinner
+
+    protected Spiel(int spielerAnzahl) {                    // Subklasse bestimmt die Spieleranzahl
+        this.spielerAnzahl = spielerAnzahl;
+    }
+
+    /**
+     * Startet ein Spiel, bei dem solange Züge ausgeführt werden,
+     * bis entweder ein Gewinner feststeht oder ein Unentschieden erreicht ist.
+     */
+    public final void starteSpiel() {                       // Schablonenmethode
+        if (ergebnis != OFFEN) {
+            System.out.println("Dieses Spiel wurde bereits gespielt.");
+            return;
+        }
+        int spieler = 0;                                    // nächster Spieler (von 0 bis Spieleranzahl - 1)
+        do {
+            int delta = macheZug(spieler);
+            ergebnis = bestimmeErgebnis(spieler);
+            spieler = (spieler + delta + spielerAnzahl) % spielerAnzahl;
+        } while (ergebnis == OFFEN);
+        druckeErgebnis(ergebnis);
+        scanner.close();
+    }
+
+    /**
+     * Erfragt den nächsten Zug des Spielers und führt ihn aus.
+     * 
+     * @param spieler Spieler, der am Zug ist (von 0 bis Spieleranzahl - 1)
+     * @return bestimmt, wer den folgenden Zug machen darf (normalerweise: +1)
+     */
+    protected abstract int macheZug(int spieler);           // Subklasse definiert Verhalten
+
+    /**
+     * Überprüft, ob das Spiel noch offen ist oder bereits beendet (Gewinner steht fest bzw. Remis).
+     * 
+     * @param spieler Spieler, der am Zug war (von 0 bis Spieleranzahl - 1)
+     * @return <code>OFFEN</code>, <code>UNENTSCHIEDEN</code> oder eine Spielernummer (von 0 bis Spieleranzahl - 1)
+     */
+    protected abstract int bestimmeErgebnis(int spieler);   // Subklasse definiert Verhalten
+
+    /**
+     * Schreibt das Ergebnis des Spiels in die Konsole.
+     * 
+     * @param ergebnis <code>OFFEN</code>, <code>UNENTSCHIEDEN</code> oder die Gewinnernummer (von 0 bis Spieleranzahl - 1)
+     */
+    protected abstract void druckeErgebnis(int ergebnis);   // Subklasse definiert Verhalten
+}
